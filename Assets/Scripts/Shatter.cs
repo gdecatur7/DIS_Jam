1179858b07d1a62000b120605cd4d6765dbfc694
Imani Finkley <ief4@cornell.edu>
2023-02-06 14:23:05 +0100

    shatter.cs

---
+++ Assets/Scripts/Shatter.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Shatter : MonoBehaviour
+{
+    public Sprite[] frames;
+    public float framesPerSecond = 5;
+    public GameObject item;
+
+    SpriteRenderer spriteRenderer;
+    int currentFrameIndex = 0;
+    float frameTimer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        frameTimer = (1f / framesPerSecond);
+        currentFrameIndex = 0;
+    }
+
+    // cycle through the sprites of explosion animation
+    void FixedUpdate()
+    {
+        frameTimer -= Time.deltaTime;
+
+        if (frameTimer <= 0)
+        {
+            currentFrameIndex++;
+            if (currentFrameIndex >= frames.Length)
+            {
+                Destroy(gameObject);
+                Instantiate(item, transform.position, Quaternion.identity);
+                return;
+            }
+            frameTimer = (1f / framesPerSecond);
+            spriteRenderer.sprite = frames[currentFrameIndex];
+        } 
+    }
+}

73d33f09e247a29bea7a731f69a76159f33cecc3
Grace Decatur <gdecatur@wustl.edu>
2023-02-06 14:31:37 +0100

    movement to the right and jumping

--- Assets/Scripts/PenguinController.cs
+++ Assets/Scripts/PenguinController.cs
@@ -5,30 +5,30 @@ using UnityEngine;
 public class PenguinController : MonoBehaviour
 {
     public float walkingSpeed = 1;
-    private float jumpingSpeed = 1;
+    private float jumpingSpeed = 3;
     private float animationFPS = 5;
     Rigidbody2D rb2D;
-    private float movementVertical;
-    private float movementHorizontal;
 
     void Start()
     {
-        movementVertical = 0;
-        movementHorizontal = walkingSpeed;
         rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var movement = new Vector3();
+        movement.x = walkingSpeed;
+        movement.y = rb2D.velocity.y;
 
-        rb2D.velocity = new Vector2(movementHorizontal, movementVertical);
 
         if (Input.GetKey(KeyCode.Space))
         {
-            movementVertical = jumpingSpeed;
+            movement.y = jumpingSpeed;
         }
+
+        rb2D.velocity = new Vector2(movement.x, movement.y);
+        transform.right = rb2D.velocity.normalized;
 
     }
 }

f481f128f9ea3d6d980751ebc3613ca57f29de97
Grace Decatur <gdecatur@wustl.edu>
2023-02-06 16:24:40 +0100

    fixed camera, added background, added sorting layers

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class Camera : MonoBehaviour
 {
     public Transform player;
     public float offsetX;
